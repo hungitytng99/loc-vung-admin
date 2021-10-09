b276e6d14b9e421e1bd98d1b1ab10efd2ed5b5c1
Manh Hung <mhungict99@gmail.com>
2021-10-09 13:10:02 +0700

    Add editor for post

---
+++ src/components/Editor/CKEditor.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import EditorType from 'ckeditor5-custom-build/build/ckeditor.js';
+import './editor.css';
+import UploadAdapterPlugin from './UploadAdapterPlugin';
+// import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
+// import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials';
+
+export default function EditorBase({ type = 'classic', placeholder = '', onTextChange, initContent, items = [] }) {
+    const [showTool, setShowTool] = useState(false);
+    const [data, setdata] = useState('');
+    const [isEditorReady, setIsEditorReady] = useState(false);
+    const itemsClassic = [
+        'heading',
+        '|',
+        'fontfamily',
+        'fontsize',
+        '|',
+        'alignment',
+        '|',
+        'fontColor',
+        'fontBackgroundColor',
+        '|',
+        'bold',
+        'italic',
+        'underline',
+        '|',
+        'numberedList',
+        'bulletedList',
+        '|',
+        'link',
+        'insertImage',
+        'mediaEmbed',
+        '|',
+        'undo',
+        'redo',
+    ];
+    const itemsInline = ['bold', 'italic', 'underline', '|', 'MathType', 'ChemType'];
+
+    useEffect(() => {
+        if (initContent && isEditorReady) {
+            setdata(initContent);
+        }
+    }, [initContent, isEditorReady]);
+
+    return (
+        <div className={(type == 'inline' && !showTool ? 'hidden-toolbar' : '') + ` ${type}` + ' compose-editor'}>
+            <CKEditor
+                editor={EditorType}
+                config={{
+                    placeholder: placeholder,
+                    // extraPlugins: [
+                    //   UploadAdapterPlugin,
+                    // ],
+                    toolbar: {
+                        items: type !== 'inline' ? itemsClassic : itemsInline,
+                    },
+                }}
+                onChange={(event, editor) => {
+                    const data = editor.getData();
+                    console.log('data: ', data);
+                    onTextChange && onTextChange(data);
+                }}
+                data={data}
+                onBlur={(event, editor) => {
+                    type == 'inline' && setShowTool(false);
+                }}
+                onFocus={(event, editor) => {
+                    type == 'inline' && setShowTool(true);
+                }}
+                onReady={(editor) => {
+                    setIsEditorReady(true);
+                }}
+            />
+        </div>
+    );
+}
--- src/containers/app/screens/Post/route.js
+++ src/containers/app/screens/Post/route.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import { MODULES } from 'app-configs';
 import { lazy } from 'react';
 import { initModules } from 'router/index';
+import CreatePost from './components/CreatePost/CreatePost';
 
 export default {
     path: MODULES.postModule.route,
@@ -10,3 +12,36 @@ export default {
         return import('.');
     }),
 };
+
+export const childRoutes = [
+    {
+        path: '/post/create',
+        exact: true,
+        isPrivate: true,
+        childComponent: <CreatePost />,
+        component: lazy(async () => {
+            await initModules([MODULES.postModule], 'app');
+            return import('.');
+        }),
+    },
+    // {
+    //     path: '/product/add-hot-product',
+    //     exact: true,
+    //     isPrivate: true,
+    //     childComponent: <AddHotProduct />,
+    //     component: lazy(async () => {
+    //         await initModules([MODULES.postModule], 'app');
+    //         return import('.');
+    //     }),
+    // },
+    // {
+    //     path: '/product/edit-product/:id',
+    //     exact: true,
+    //     isPrivate: true,
+    //     childComponent: <EditProduct />,
+    //     component: lazy(async () => {
+    //         await initModules([MODULES.postModule], 'app');
+    //         return import('.');
+    //     }),
+    // },
+];
