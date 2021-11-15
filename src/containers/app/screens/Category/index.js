2426b945dc4782e9b7f446df12f8660166722e79
crytal99 <thanthanh160799@gmail.com>
2021-11-15 22:55:07 +0700

    fix all lable category 3

--- src/containers/app/screens/Category/index.js
+++ src/containers/app/screens/Category/index.js
@@ -9,7 +9,7 @@ const Category = (props) => {
         <Suspense fallback={<Spin />}>
             <Switch>
                 <Route exact path="/category">
-                    <ListProduct />
+                    <Listcategory />
                 </Route>
                 {childRoutes.map((route) => (
                     <Route exact={route.exact} path={route.path}>
