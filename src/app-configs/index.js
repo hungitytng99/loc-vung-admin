9b085bde9c78172a7d46a0147a399eb58b439ebc
Manh Hung <mhungict99@gmail.com>
2021-11-17 11:25:01 +0700

    Fix env for deploy

--- src/app-configs/index.js
+++ src/app-configs/index.js
@@ -12,7 +12,10 @@ import {
 // SWAGGER:
 // http://locvungshop.southeastasia.cloudapp.azure.com:4000/api-docs/
 export const Configs = {
-    BASE_API: 'http://locvungshop.southeastasia.cloudapp.azure.com:4000/api/v1',
+    BASE_API:
+        process.env.REACT_APP_ENV === 'develop'
+            ? 'http://locvungshop.southeastasia.cloudapp.azure.com:4000/api/v1'
+            : 'https://locvungshop.southeastasia.cloudapp.azure.com:4000/api/v1',
     DOMAIN: '',
 
     CURRENT_PAGE: 1,
