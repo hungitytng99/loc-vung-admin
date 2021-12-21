bc4db8c614d9e3475ab29f2a5272d4d94065cb48
Manh Hung <mhungict99@gmail.com>
2021-12-22 01:04:12 +0700

    Add cuscom ck-editor for build

--- src/components/Editor/CKEditor.js
+++ src/components/Editor/CKEditor.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import CKEditorVi from 'ckeditor5-custom-build/build/ckeditor';
+import CKEditorVi from 'ckeditor5-custom-build/buildck/ckeditor';
 import UploadAdapterPlugin from './UploadAdapterPlugin';
 // import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
 // import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials';
